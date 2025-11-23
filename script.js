fedcfae0c4fe76e905bafd7cda69c8097f5c4575
SKPTP-9 <tomas.duncan.ap@gmail.com>
2025-11-23 12:44:25 -0300

    9vo commit, mejorando el responsive

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ burger.addEventListener('click', function() {
     burger.classList.toggle('active')
     nav.classList.toggle('active') 
 })
+
+let aside_menu = document.querySelector('.aside-menu')
+let aside = document.querySelector('aside')
+aside_menu.addEventListener('click', function() {
+    aside_menu.classList.toggle('aside_open')
+    aside.classList.toggle('aside_open') 
+})
